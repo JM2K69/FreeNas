c356dd4a7332ccddcb15d908ec07130b7492df3f
JM2K69 <bezettorres.jerome@neuf.fr>
2019-10-17 12:41:54 +0200

    Fix typo

--- Freenas/class/Classes FreeNas.ps1
+++ Freenas/class/Classes FreeNas.ps1
@@ -5,18 +5,18 @@
     [String]$Uri
     [String]$AuthToken
     [System.Collections.ArrayList]$ListDisk
-        
+
     FreeNasServer()
-    {}
-    
+    { }
+
     FreeNasServer ([String]$NomCommun, [ipaddress]$ServerAddress)
     {
         $this.NomCommun = $NomCommun
         $this.ServerAddress = $ServerAddress
-        $this.Uri =  = "http://$ServerAddress/api/v1.0"
+        $this.Uri = = "http://$ServerAddress/api/v1.0"
     }
 
-   [void] connect()
+    [void] connect()
     {
         Invoke-RestMethod -Uri $This.Uri  -Method Get -SessionVariable $This.AuthToken -Credential (Get-Credential)
 
@@ -24,7 +24,7 @@
 
     [System.Collections.ArrayList] getdisks()
     {
-        
+
         return $This.ListDisk
     }
 
@@ -32,47 +32,47 @@
 
 class DDisk
 {
-   [String]$Backend = "/storage/disk/"
-   [String]$Uri = "http://$ServerAddress/api/v1.0"
-   [String]$AuthToken
+    [String]$Backend = "/storage/disk/"
+    [String]$Uri = "http://$ServerAddress/api/v1.0"
+    [String]$AuthToken
 
-   DDisk([String]$Uri,[String]$AuthToken)
-   {
-      $this.Uri = $Uri
-      $This.AuthToken = $AuthToken
-   }
-
-    
-   [System.Collections.ArrayList]getlist()
-   {
-   
-    $results = Invoke-RestMethod -Uri $this.Uri -WebSession $this.AuthToken -Method Get
-   
-     foreach ($disk in $results)
+    DDisk([String]$Uri, [String]$AuthToken)
     {
-        $Name = ($disk.disk_name)
-        $Size_GB = ([Math]::Round($disk.disk_size / 1024 / 1024 / 1024, 2))
-        Write-Verbose " Find the disk $name with the size $Size_GB  "
-        $diskFound = [Disk]::new()
-        [DisK]@{
-            Name    = ($disk.disk_name)
-            Size_GB = ([Math]::Round($disk.disk_size / 1024 / 1024 / 1024, 2))
-        }
+        $this.Uri = $Uri
+        $This.AuthToken = $AuthToken
     }
 
 
-   }
+    [System.Collections.ArrayList]getlist()
+    {
 
-   save([Disk]$D)
-   {
-   }
-   update([Disk]$D)
-   {
-   }
+        $results = Invoke-RestMethod -Uri $this.Uri -WebSession $this.AuthToken -Method Get
 
-   delete([Disk]$D)
-   {
-   }
+        foreach ($disk in $results)
+        {
+            $Name = ($disk.disk_name)
+            $Size_GB = ([Math]::Round($disk.disk_size / 1024 / 1024 / 1024, 2))
+            Write-Verbose " Find the disk $name with the size $Size_GB  "
+            $diskFound = [Disk]::new()
+            [DisK]@{
+                Name    = ($disk.disk_name)
+                Size_GB = ([Math]::Round($disk.disk_size / 1024 / 1024 / 1024, 2))
+            }
+        }
+
+
+    }
+
+    save([Disk]$D)
+    {
+    }
+    update([Disk]$D)
+    {
+    }
+
+    delete([Disk]$D)
+    {
+    }
 }
 
 class Disk
@@ -82,8 +82,6 @@ class Disk
 
     Disk()
     {
-    
-        
-    
+
     }
 }
