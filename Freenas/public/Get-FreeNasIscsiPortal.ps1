7d70c4e409be2a6f94afebecb992863afa9cf874
Jérôme Bezet-Torres <JM2K69@users.noreply.github.com>
2019-10-29 21:26:34 +0100

    Update URI

--- Freenas/public/Get-FreeNasIscsiPortal.ps1
+++ Freenas/public/Get-FreeNasIscsiPortal.ps1
@@ -10,7 +10,7 @@
     }
     Process
     {
-        $Uri = "http://$script:SrvFreenas/api/v1.0/services/iscsi/portal/"
+        $Uri = "api/v1.0/services/iscsi/portal/"
 
         $result = Invoke-FreeNasRestMethod -Uri $Uri -Method Get
 
