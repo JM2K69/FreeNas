0fe1b3aca70fafd7911e4b39c9a1ed04e31b2b98
Jérôme Bezet-Torres <JM2K69@users.noreply.github.com>
2019-10-29 21:36:57 +0100

    Update URI

--- Freenas/public/New-FreeNasIscsiInitiator.ps1
+++ Freenas/public/New-FreeNasIscsiInitiator.ps1
@@ -22,14 +22,12 @@
     }
     Process
     {
-        $Uri = "http://$script:SrvFreenas/api/v1.0/services/iscsi/authorizedinitiator/"
+        $Uri = "api/v1.0/services/iscsi/authorizedinitiator/"
 
         $Obj = [Ordered]@{
             iscsi_target_initiator_initiators   = $AuthInitiators
             iscsi_target_initiator_auth_network = $AuthNetwork
         }
-        
-        $post = $Obj |ConvertTo-Json
 
         $response = Invoke-FreeNasRestMethod -method Post -body $Obj -Uri $Uri
 
