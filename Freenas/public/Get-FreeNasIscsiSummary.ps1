8045f977a205a49ceefc6a581d437ea850032506
JM2K69 <bezettorres.jerome@neuf.fr>
2019-11-07 14:44:36 +0100

    Some Change

--- Freenas/public/Get-FreeNasIscsiSummary.ps1
+++ Freenas/public/Get-FreeNasIscsiSummary.ps1
@@ -1,4 +1,5 @@
-﻿function Get-FreeNasIscsiSummary {
+﻿function Get-FreeNasIscsiSummary
+{
     [Alias()]
     Param
     ()
@@ -9,9 +10,7 @@
     }
     Process
     {
-        $Conf_Iscsi = Get-FreenasIscsiConf
         $Extent_Iscsi = Get-FreenasIscsiExtent
-        $Initiator_Iscsi = Get-FreenasIscsiInitiator
         $Association = Get-FreenasIscsiAssociat2Extent -Output Name
         $Target_Iscsi = Get-FreenasIscsiTarget
 
@@ -20,30 +19,36 @@
 
         Write-Host "Your Freenas Server" -NoNewline
         Write-Host " $script:SrvFreenas" -NoNewline -ForegroundColor Cyan
-        Write-Host " Iscsi Configuration : "
-        $Conf_Iscsi
+        Write-Host ""
+        Write-Host "Iscsi Configuration : " -ForegroundColor Yellow
+        Get-FreenasIscsiConf
 
-        Write-host "The Iscsi Portal : "
+        Write-host "The Iscsi Portal : " -ForegroundColor Yellow
         Get-FreenasIscsiPortal
+        Write-host "The Target Iscsi :" -ForegroundColor Yellow
+        write-host ""
+        get-FreenasIscsiInitiator
+        Write-host "The Iscsi Extent : " -ForegroundColor Yellow
 
         $TotalSize = ""
-        for ($i = 0; $i -lt $Extent_Iscsi.Count; $i++) {
+        for ($i = 0; $i -lt $Extent_Iscsi.Count; $i++)
+        {
             [int]$Size = [Math]::Round($Extent_Iscsi[$i].Extent_Size , 2)
             [Int]$TotalSize = $TotalSize + $Size
         }
         Write-host "The Server have"$($Extent_Iscsi).count"Extent(s) with a total size" -NoNewline
-        Write-Host " $TotalSize " -NoNewline -ForegroundColor Cyan
+        Write-Host " $TotalSize " -NoNewline -ForegroundColor Green
         Write-Host "GB"
-        Write-host "The Iscsi Extent Type :"
-        $Extent_Iscsi | FT
-        Write-host "The Target Iscsi :"
-
+        Write-host "The Iscsi Extent Type : " -ForegroundColor Yellow
+        Get-FreenasIscsiExtent | FT
+        Write-host "The Target Iscsi :" -ForegroundColor Yellow
         $Target_Iscsi | ft
-        Write-host "The Association Target with Extend :"
+        Write-host "The Association Target with Extend :" -ForegroundColor Yellow
 
         $Association | ft
     }
-    End {
+    End
+    {
         New-banner -Text "End Summary"  -Online -FontColor Red
 
     }
---
+++ dev/APIV2.ps1
@@ -0,0 +1,217 @@
+﻿function Set-FreeNasCipherSSL {
+
+    # Hack for allowing TLS 1.1 and TLS 1.2 (by default it is only SSL3 and TLS (1.0))
+    $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+}
+function Set-FreeNasUntrustedSSL
+{
+
+    # Hack for allowing untrusted SSL certs with https connections
+    Add-Type -TypeDefinition @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
+
+}
+
+function Show-FreeNasException() {
+    Param(
+        [parameter(Mandatory = $true)]
+        $Exception
+    )
+
+    #Check if certificate is valid
+    if ($Exception.Exception.InnerException) {
+        $exceptiontype = $Exception.Exception.InnerException.GetType()
+        if ("AuthenticationException" -eq $exceptiontype.name) {
+            Write-Warning "Invalid certificat (Untrusted, wrong date, invalid name...)"
+            Write-Warning "Try to use Connect-FreeNasServer -SkipCertificateCheck for connection"
+            throw "Unable to connect (certificate)"
+        }
+    }
+
+    If ($Exception.Exception.Response) {
+        if ("Desktop" -eq $PSVersionTable.PSEdition) {
+            $result = $Exception.Exception.Response.GetResponseStream()
+            $reader = New-Object -TypeName System.IO.StreamReader($result)
+            $responseBody = $reader.ReadToEnd()
+            $responseJson =  $responseBody | ConvertFrom-Json
+        }
+
+        Write-Warning "The FreeNas  API sends an error message:"
+        Write-Warning "Error description (code): $($Exception.Exception.Response.StatusDescription) ($($Exception.Exception.Response.StatusCode.Value__))"
+        if ($responseBody) {
+            if ($responseJson.message) {
+                Write-Warning "Error details: $($responseJson.message)"
+            }
+            else {
+                Write-Warning "Error details: $($responseBody)"
+            }
+        }
+        elseif ($Exception.ErrorDetails.Message) {
+            Write-Warning "Error details: $($Exception.ErrorDetails.Message)"
+        }
+    }
+}
+function Connect-FreeNasServer
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Description d’aide Freenas
+        [Parameter(Mandatory = $true)]
+        [Alias("Freenas")]
+        $Server,
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("v1.0", "v2.0")]
+        [Alias("Api")]
+        $ApiVersion,
+        [Parameter(Mandatory = $false)]
+        [String]$Username,
+        [Parameter(Mandatory = $false)]
+        [SecureString]$Password,
+        [Parameter(Mandatory = $false)]
+        [PSCredential]$Credentials,
+        [Parameter(Mandatory = $false)]
+        [switch]$httpOnly = $false,
+        [Parameter(Mandatory = $false)]
+        [switch]$SkipCertificateCheck = $false,
+        [Parameter(Mandatory = $false)]
+        [ValidateRange(1, 65535)]
+        [int]$port
+    )
+
+    Begin
+    {
+        Try
+        {
+            #New-banner -Text "FreeNas 2.0" -Online -ErrorAction stop
+        }
+        Catch
+        {
+            #New-banner -Text "FreeNas 2.0"
+        }
+
+    }
+    Process
+    {
+        $Script:SrvFreenas = $Server
+
+
+        #If there is a password (and a user), create a credentials
+        if ($Password)
+        {
+            $securecurepassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+            $Credentials = New-Object -TypeName System.Management.Automation.PSCredential($Username, $securecurepassword)
+        }
+        #Not Credentials (and no password)
+        if ($NULL -eq $Credentials)
+        {
+            $Credentials = Get-Credential -Message 'Please enter administrative credentials for your FreeNas'
+        }
+        $cred = $Credentials.username + ":" + $Credentials.GetNetworkCredential().Password
+        $base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($cred))
+        #headers, We need to have Content-type set to application/json...
+        $script:headers = @{ Authorization = "Basic " + $base64; "Content-type" = "application/json" }
+        $script:invokeParams = @{ UseBasicParsing = $true; SkipCertificateCheck = $SkipCertificateCheck }
+
+        if ("Desktop" -eq $PSVersionTable.PsEdition)
+        {
+            #Remove -SkipCertificateCheck from Invoke Parameter (not supported <= PS 5)
+            $invokeParams.remove("SkipCertificateCheck")
+        }
+
+        if ($httpOnly)
+        {
+            if (!$port)
+            {
+                $port = 80
+            }
+            switch ($ApiVersion)
+            {
+                'v1.0' {$uri = "http://${Server}:${port}/api/v1.0/system/version/"}
+                'v2.0' {$uri = "http://${Server}:${port}/api/v2.0/system/info"}
+                Default {}
+            }
+
+        }
+        else
+        {
+            if (!$port)
+            {
+                $port = 443
+            }
+            #for PowerShell (<=) 5 (Desktop), Enable TLS 1.1, 1.2 and Disable SSL chain trust
+            if ("Desktop" -eq $PSVersionTable.PsEdition)
+            {
+                Write-Verbose -Message "Desktop Version try to Enable TLS 1.1 and 1.2"
+                #Enable TLS 1.1 and 1.2
+                Set-FreeNasCipherSSL
+                if ($SkipCertificateCheck)
+                {
+                    Write-Verbose -Message "Disable SSL chain trust"
+
+                    #Disable SSL chain trust...
+                    Set-FreeNasuntrustedSSL
+                }
+
+            }
+            switch ($ApiVersion)
+            {
+                'v1.0' {$uri = "http://${Server}:${port}/api/v1.0/system/version/"}
+                'v2.0' {$uri = "http://${Server}:${port}/api/v2.0/system/info"}
+                Default {}
+            }
+
+        }
+
+        $script:port = $port
+        $script:httpOnly = $httpOnly
+        $Script:ApiVersion = $ApiVersion
+
+        try
+        {
+            $result = Invoke-RestMethod -Uri $uri -Method Get -SessionVariable Freenas_S -headers $headers @invokeParams
+        }
+        catch
+        {
+            Show-FreeNasException -Exception $_
+            throw "Unable to connect"
+        }
+
+        if ($null -eq $result.version )
+        {
+            throw "Unable to get data"
+        }
+
+                    switch ($ApiVersion)
+            {
+                'v1.0' {Write-Host "Welcome on"$result.name"-"$result.fullversion""}
+                'v2.0' {Write-Host "Welcome on"$result.name"-"$result.version"-"$result.system_product""}
+                Default {}
+            }
+
+        $Script:Session = $Freenas_S
+
+
+    }
+    End
+    {
+
+    }
+}
+
+Connect-FreeNasServer -Server 192.168.0.26 -ApiVersion v2.0 -httpOnly 
