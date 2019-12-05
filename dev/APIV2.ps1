e9f79b85cb76b66af026c85e4a9610b14236a103
JM2K69 <bezettorres.jerome@neuf.fr>
2019-12-05 16:26:19 +0100

    Add function api v2.0

--- dev/APIV2.ps1
+++ dev/APIV2.ps1
@@ -626,11 +626,132 @@ function Get-FreeNasInterface
     }
 }
 
+function Get-FreeNasInternalCA
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    ()
+
+
+    Begin
+    { }
+    Process
+    {
+        $Uri = "api/v2.0/certificateauthority"
+
+        $result = Invoke-FreeNasRestMethod -Uri $Uri -Method Get
+        return $result
+    }
+    End
+    {
+    }
+}
+
+function Get-FreeNasIscsiConf
+{
+    Param
+    ( )
+
+    Begin
+    {
+
+    }
+    Process
+    {
+        $Uri = "api/v2.0/iscsi/global"
+
+        $result = Invoke-FreeNasRestMethod -Uri $Uri -Method Get
+
+    }
+    End
+    {
+        $IscsiConf = New-Object -TypeName System.Collections.ArrayList
+
+        $temp = New-Object -TypeName PSObject
+        $temp | Add-Member -MemberType NoteProperty -Name "Id" -Value $result.id
+        $temp | Add-Member -MemberType NoteProperty -Name "Base Name" -Value $result.basename
+        $temp | Add-Member -MemberType NoteProperty -Name "ISNS Server" -Value $result.isns_servers
+        $temp | Add-Member -MemberType NoteProperty -Name "Pool available space Threshold (%)" -Value $result.pool_avail_threshold
+        $temp | Add-Member -MemberType NoteProperty -Name "Alua" -Value $result.alua
+
+
+        $IscsiConf.Add($temp) | Out-Null
+
+        return $IscsiConf | fl
+    }
+}
+
+function Get-FreeNasIscsiExtent
+{
+
+    Param
+    ()
+
+
+    Begin
+    {
+
+    }
+    Process
+    {
+        $Uri = "api/v2.0/iscsi/extent"
+        $result = Invoke-FreeNasRestMethod -Uri $Uri -Method Get
+
+        $Extent = New-Object -TypeName System.Collections.ArrayList
+        $temp = New-Object -TypeName System.Object
+
+        if ($null -eq $result.Count)
+        {
+
+            $temp | Add-Member -MemberType NoteProperty -Name "Id" -Value  "$($result.id)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent Type" -Value "$($result.type)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent Name" -Value  "$($result.name)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent path" -Value  "$($result.path)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent Block Size" -Value  "$($result.blocksize)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent Speed Type" -Value "$($result.rpm)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent Naa" -Value "$($result.naa)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent enabled" -Value "$($result.enabled)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent disk" -Value "$($result.disk)"
+            $temp | Add-Member -MemberType NoteProperty -Name "Extent Xen" -Value "$($result.xen)"
+
+            $Extent.Add($temp) | Out-Null
+
+        }
+        else
+        {
+            for ($i = 0; $i -lt $result.Count; $i++)
+            {
+                $temp | Add-Member -MemberType NoteProperty -Name "Id" -Value  "$($result[$i].id)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent Type" -Value "$($result[$i].type)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent Name" -Value  "$($result[$i].name)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent path" -Value  "$($result[$i].path)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent Block Size" -Value  "$($result[$i].blocksize)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent Speed Type" -Value "$($result[$i].rpm)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent Naa" -Value "$($result[$i].naa)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent enabled" -Value "$($result[$i].enabled)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent disk" -Value "$($result[$i].disk)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Extent Xen" -Value "$($result[$i].xen)"
+                $Extent.Add($temp) | Out-Null
+            }
+
+        }
+
+        return $Extent
+
+
+    }
+    End
+    { }
+}
+
 
 ###########TEST#######################################################
-Connect-FreeNasServer -Server 192.168.0.25 -ApiVersion v2.0 -httpOnly
+Connect-FreeNasServer -Server 192.168.0.27 - -httpOnly
 Get-FreeNasCertificate -Verbose
 Get-FreeNasDisk -Verbose
 Get-FreeNasDiskUnsed -Verbose
 Get-FreeNasGlobalConfig
 Get-FreeNasInterface
+Get-FreeNasIscsiConf
+Get-FreeNasIscsiExtent
