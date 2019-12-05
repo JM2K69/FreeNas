63489aabfd447a4d2b55053bda1383777c7b744b
JM2K69 <bezettorres.jerome@neuf.fr>
2019-12-05 16:52:37 +0100

    Add Function Get To Apiv2

--- dev/APIV2.ps1
+++ dev/APIV2.ps1
@@ -248,7 +248,7 @@ function Connect-FreeNasServer
                 }
 
             }
-            $uri = "http://${Server}:${port}/api/v2.0/system/info" 
+            $uri = "http://${Server}:${port}/api/v2.0/system/info"
 
         }
 
@@ -745,6 +745,109 @@ function Get-FreeNasIscsiExtent
     { }
 }
 
+function Get-FreeNasIscsiInitiator
+{
+    Param
+    ( )
+
+
+    Begin
+    {
+
+    }
+    Process
+    {
+        $Uri = "api/v2.0/iscsi/initiator"
+        $result = Invoke-FreeNasRestMethod -Uri $Uri -Method Get
+
+        $initiator = New-Object -TypeName System.Collections.ArrayList
+        $temp = New-Object -TypeName System.Object
+
+        if ($null -eq $result.Count)
+        {
+            $temp | Add-Member -MemberType NoteProperty -Name "Id" -Value $result.id
+            $temp | Add-Member -MemberType NoteProperty -Name "Tag" -Value $result.tag
+            $temp | Add-Member -MemberType NoteProperty -Name "Initiator" -Value $result.initiators
+            $temp | Add-Member -MemberType NoteProperty -Name "Auth Network" -Value $result.auth_network
+            $temp | Add-Member -MemberType NoteProperty -Name "Comments" -Value $result.comment
+            $initiator.Add($temp) | Out-Null
+
+        }
+        else
+        {
+            for ($i = 0; $i -lt $result.Count; $i++)
+            {
+                $temp = New-Object -TypeName System.Object
+                $temp | Add-Member -MemberType NoteProperty -Name "Id" -Value "$($result[$i].id)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Tag" -Value "$($result[$i].tag)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Initiator" -Value "$($result[$i].initiators)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Auth Network" -Value "$($result[$i].auth_network)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Comments" -Value "$($result[$i].comment)"
+                $initiator.Add($temp) | Out-Null
+            }
+
+        }
+
+        return $initiator
+
+
+    }
+    End
+    { }
+}
+
+function Get-FreeNasIscsiPortal
+{
+    Param
+    ( )
+
+
+    Begin
+    {
+
+    }
+    Process
+    {
+        $Uri = "api/v2.0/iscsi/portal"
+
+        $result = Invoke-FreeNasRestMethod -Uri $Uri -Method Get
+
+        $Obj = New-Object -TypeName System.Collections.ArrayList
+        $temp = New-Object -TypeName System.Object
+
+        if ($null -eq $result.Count)
+        {
+            $temp | Add-Member -MemberType NoteProperty -Name "Id" -Value $result.id
+            $temp | Add-Member -MemberType NoteProperty -Name "Tag" -Value $result.tag
+            $temp | Add-Member -MemberType NoteProperty -Name "Listen" -Value $result.listen.ip
+            $temp | Add-Member -MemberType NoteProperty -Name "Port" -Value $result.listen.port
+            $temp | Add-Member -MemberType NoteProperty -Name "Discovery authmethod" -Value $result.discovery_authmethod
+            $temp | Add-Member -MemberType NoteProperty -Name "Discovery authgroup" -Value $result.discovery_authgroup
+            $Obj.Add($temp) | Out-Null
+
+        }
+        else
+        {
+            for ($i = 0; $i -lt $result.Count; $i++)
+            {
+                $temp = New-Object -TypeName System.Object
+                $temp | Add-Member -MemberType NoteProperty -Name "Id" -Value "$($result[$i].id)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Tag" -Value "$($result[$i].tag)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Listen" -Value "$($result[$i].listen.ip)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Port" -Value "$($result[$i].listen.port)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Discovery authmethod" -Value "$($result[$i].discovery_authmethod)"
+                $temp | Add-Member -MemberType NoteProperty -Name "Discovery authgroup" -Value "$($result[$i].discovery_authgroup)"
+                $Obj.Add($temp) | Out-Null
+            }
+
+        }
+        return $Obj
+    }
+    End
+    { }
+}
+
+
 
 ###########TEST#######################################################
 Connect-FreeNasServer -Server 192.168.0.27 - -httpOnly
@@ -755,3 +858,5 @@ Get-FreeNasGlobalConfig
 Get-FreeNasInterface
 Get-FreeNasIscsiConf
 Get-FreeNasIscsiExtent
+Get-FreeNasIscsiInitiator
+Get-FreeNasIscsiPortal
